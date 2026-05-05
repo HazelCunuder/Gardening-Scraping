e0df902f185fdcb580c3beabec08f87195b40dd3
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-05-05 15:33:13 +0200

    feat: add cleaning data pipeline

--- src/gardening_scraper/gardening_scraper/pipelines.py
+++ src/gardening_scraper/gardening_scraper/pipelines.py
@@ -6,8 +6,26 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import re
 
 
 class GardeningScraperPipeline:
     def process_item(self, item, spider):
+        return item
+
+class ProductPipeline:
+
+    def process_item(self, item, spider):
+        for field in item:
+            if isinstance(item[field], str):
+                item[field] = item[field].strip()
+
+        if item.get('price_concat'):
+            item['price_concat'] = float(item['price_concat'].replace('€', '.'))
+
+        if item.get('product_id'):
+            item['product_id'] = int(re.sub(r'\D', '', item['product_id']))
+        if item.get('product_code'):
+            item['product_code'] = int(re.sub(r'\D', '', item['product_code']))
+
         return item
