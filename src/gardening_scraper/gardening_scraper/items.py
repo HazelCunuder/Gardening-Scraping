ac880237c417afa035202971fc58ebc33020d8f8
Hazel Cunuder <hazel.claire.lee@gmail.com>
2026-05-05 14:30:19 +0200

    feat: add items

--- src/gardening_scraper/gardening_scraper/items.py
+++ src/gardening_scraper/gardening_scraper/items.py
@@ -10,3 +10,14 @@ class GardeningScraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ProductsItem(scrapy.Item):
+    url = scrapy.Field()
+    name = scrapy.Field()
+    price_euros = scrapy.Field()
+    price_cents = scrapy.Field()
+    product_id = scrapy.Field()
+    product_code = scrapy.Field()
+    product_category = scrapy.Field()
+    promotions = scrapy.Field()
+        
